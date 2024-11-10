1798dbaa52093901e6fe7b3dfb3c7dae56c45056
Karson Nichols <38633639+nicholsk18@users.noreply.github.com>
2024-11-10 03:49:53 +0000

    update to newer version plus buddy boss adition

--- bp-timezones.php
+++ bp-timezones.php
@@ -2,21 +2,37 @@
 
 function bp_custom_message_date( $bp_time ) {
 	global $messages_template;
-	
+
 	$member_zone = bp_get_member_profile_data('field=Time Zone');
 
-	if ( $member_zone != '' ) {	
-		$timezone = bp_timezones_lookup( $member_zone );	
-		$timestamp = '@' . strtotime($messages_template->thread->last_message_date); 
+	if ( $member_zone != '' ) {
+		$timezone = bp_timezones_lookup( $member_zone );
+		$timestamp = '@' . strtotime($messages_template->thread->last_message_date);
 		$dt = new DateTime( $timestamp );
 		$dt->setTimeZone( new DateTimeZone($timezone) );
 		return $dt->format('M j, Y, g:i a'); /* format the displayed timestamp however you want */
-	} 
-	else 
+	}
+	else
 		return $bp_time;
 
 }
 add_filter( 'bp_get_message_thread_last_post_date', 'bp_custom_message_date', 11, 1 );
+
+// customize buddy boss time sent in to user local timezone if set
+function bb_custom_chat_date($bp_time) {
+	global $thread_template;
+
+	$member_zone = bp_get_member_profile_data('field=Time Zone');
+	if ( $member_zone != '' ) {
+		$timezone = bp_timezones_lookup( $member_zone );
+		$dt = new DateTime( $thread_template->message->date_sent );
+		$dt->setTimeZone( new DateTimeZone($timezone) );
+		return $dt->format('M j, Y, g:i a');
+	}
+
+	return $bp_time;
+}
+add_filter( 'bb_get_the_thread_message_sent_time', 'bb_custom_chat_date', 11, 1 );
 
 
 function bp_timezones_lookup( $member_zone ) {
@@ -25,6 +41,3 @@ function bp_timezones_lookup( $member_zone ) {
 
 	return array_search($member_zone, $timezones);
 }
-
-
-?>
